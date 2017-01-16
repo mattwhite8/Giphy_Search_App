14aaf4917540de3c78dc305e4aa80493333501ed
Matt White <mattwhite1337@gmail.com>
2017-01-15 19:17:38 -0500

    added autocomplete to search

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ var searchButton = document.getElementById("searchButton");
 var searchBox = document.getElementById("search");
 var clearButton = document.getElementById("clear");
 var giphyData = JSON.parse(localStorage.getItem("giphyResults")) || [];
+var suggestCallBack; // global var for autocomplete jsonp
+
 
 function createButtons(){
 
@@ -116,5 +118,26 @@ $( document ).ready(function(){
     buttonData = ['cat','dog','bird','horse'];
     createButtons();
   })
+
+  $("#search").autocomplete({
+      source: function(request, response) {
+          $.getJSON("http://suggestqueries.google.com/complete/search?callback=?",
+              {
+                "hl":"en", // Language
+                "jsonp":"suggestCallBack", // jsonp callback function name
+                "q":request.term, // query term
+                "client":"youtube" // force youtube style response, i.e. jsonp
+              }
+          );
+          suggestCallBack = function (data) {
+              var suggestions = [];
+              $.each(data[1], function(key, val) {
+                  suggestions.push({"value":val[0]});
+              });
+              suggestions.length = 5; // prune suggestions list to only 5 items
+              response(suggestions);
+          };
+      },
+  });  
   
 });
