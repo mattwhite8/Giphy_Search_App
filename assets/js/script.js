2ef5b98a6f186ad05060f2886b799fe932fa0610
Matt White <mattwhite1337@gmail.com>
2017-01-15 19:19:23 -0500

    changed http to https

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,7 +121,7 @@ $( document ).ready(function(){
 
   $("#search").autocomplete({
       source: function(request, response) {
-          $.getJSON("http://suggestqueries.google.com/complete/search?callback=?",
+          $.getJSON("https://suggestqueries.google.com/complete/search?callback=?",
               {
                 "hl":"en", // Language
                 "jsonp":"suggestCallBack", // jsonp callback function name
